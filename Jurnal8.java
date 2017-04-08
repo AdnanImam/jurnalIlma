2d2872c60792526e9a29c875af5d3d3271a172ff
Ilma Rinjani <rinjaniilma@gmail.com>
2017-04-08 11:37:12 +0700

    Update Jurnal8.java

--- Jurnal8.java
+++ Jurnal8.java
@@ -8,9 +8,9 @@ public class Jurnal8 {
 
  
     public static void main(String[] args) {
-         int a=0,b=1,hasil=0,c;
+         int a=1,b=1,hasil=0,c;
         Scanner input = new Scanner (System.in);
-   System.out.print ("Masukan Deret Fibonacci  : ");
+   System.out.print ("Masukkan angka   : ");
         c = input.nextInt ();
         for(int i=1; i<=c; i++){            
     a=b;
